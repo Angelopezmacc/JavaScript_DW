b1303fa182d063eaf9a4e407b3ab12b8fa83e915
angelopez_macc <angelopezmacc@gmail.com>
2023-11-05 16:37:58 -0500

    Primera clase de JS
    Breve introducción al lenguaje.

---
+++ intro.js
@@ -0,0 +1,11 @@
+/* Se definen variables */
+
+var a = 5 /* ya no se usa*/
+
+/* Así se definen las variables actualmente */
+let b = 7
+const c = 0 /* así se define una constante */
+
+
+console.log("Hola Mundo")
+alert("Hola")
